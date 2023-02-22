e0dab632ca98bc4bbcbf04a75eba76987c2046fc
Stephen Scally <stevescally@gmail.com>
2023-02-22 08:59:34 -0500

    Added the GPLv3 License explicitly.

--- dvd_digitizing/dvd_monitor.py
+++ dvd_digitizing/dvd_monitor.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
 ''' 
+GPLv3 License
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see https://www.gnu.org/licenses/.
+
+Copyright (C) 2022 Stephen Scally
+'''
+
+'''
 This script accepts a process id to monitor and then will alert
 via text message when the job no longer is running. This is not 
 to indicate that the dvd ripping was successfully but more so to
--- ebook_rename/ebook_rename.py
+++ ebook_rename/ebook_rename.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-'''
+
+''' 
+GPLv3 License
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see https://www.gnu.org/licenses/.
+
+Copyright (C) 2022 Stephen Scally
+
 Imports
 -------
 os:       Module used for directory structure, file input/output.
@@ -9,8 +25,6 @@ hashlib:  Module to generate sha256 hashes
 argparse: Module providing option input values and help output.
 rich.*:   Module used to provide status feedback and functionality
           to the script.
-
-
 '''
 
 import os
